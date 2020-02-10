d5a105dac24ab15cee176e78767d39adc50d9d74
okeyliu <okeyliu@163.com>
2020-02-10 23:09:24 +0800

    init

---
+++ src/main/java/site/okliu/newvision/NewvisionApplication.java
@@ -0,0 +1,13 @@
+package site.okliu.newvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewvisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewvisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/okliu/newvision/NewvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package site.okliu.newvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewvisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
