7daa508bc485d4418002b7bfaccbf096b08d061d
okeyliu <okeyliu@163.com>
2020-02-11 02:28:29 +0800

    change AppConstant to Spring @Value properties

--- src/main/java/site/okliu/newvision/controller/AuthorizeController.java
+++ src/main/java/site/okliu/newvision/controller/AuthorizeController.java
@@ -1,10 +1,10 @@
 package site.okliu.newvision.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import site.okliu.newvision.constant.AppConstant;
 import site.okliu.newvision.dto.AccessTokenDTO;
 import site.okliu.newvision.dto.GithubUser;
 import site.okliu.newvision.provider.GithubProvider;
@@ -14,17 +14,24 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
-    
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.redirect_uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name="state") String state){
         System.out.println("code = " + code);
         System.out.println("state = " + state);
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id(AppConstant.CLIENT_ID);
-        accessTokenDTO.setClient_secret(AppConstant.CLIENT_SECRET);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri(AppConstant.REDIRECT_URI);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
--- src/main/java/site/okliu/newvision/provider/GithubProvider.java
+++ src/main/java/site/okliu/newvision/provider/GithubProvider.java
@@ -3,7 +3,6 @@ package site.okliu.newvision.provider;
 import com.alibaba.fastjson.JSON;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
-import site.okliu.newvision.constant.AppConstant;
 import site.okliu.newvision.dto.AccessTokenDTO;
 import site.okliu.newvision.dto.GithubUser;
 
@@ -35,7 +34,7 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(AppConstant.USER_INFO_BASE_URI+"?access_token="+accessToken)
+                .url("https://api.github.com/user?access_token="+accessToken)
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String info = response.body().string();
